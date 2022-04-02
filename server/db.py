b36c43e6768f90dd27fbf41eb1fbd27b0b21981d
badger-py <yan090409@gmail.com>
2022-04-02 21:16:34 +0400

    Deploy ready app (without special scripts)

--- server/db.py
+++ server/db.py
@@ -1,8 +1,12 @@
+from os import getenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///sql_app.db"
+SQLALCHEMY_DATABASE_URL = getenv(
+    "SQLALCHEMY_DATABASE_URL", default="sqlite:///sql_app.db")
+# SQLALCHEMY_DATABASE_URL = "sqlite:///sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
