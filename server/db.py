20583fbea0019f4e3938539d676b40aacdee2a28
badger-py <yan090409@gmail.com>
2022-04-03 11:07:36 +0400

    Fix bug check_same_thread

--- server/db.py
+++ server/db.py
@@ -9,9 +9,14 @@ SQLALCHEMY_DATABASE_URL = getenv(
 # SQLALCHEMY_DATABASE_URL = "sqlite:///sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
