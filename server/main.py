9c5efdd1452837f13bf3056d5330e57d73282dad
badger-py <yan090409@gmail.com>
2022-04-03 10:32:05 +0400

    Some fix to deploy

--- server/main.py
+++ server/main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from endpoints.websites import websites_endpoint
 
 app = FastAPI(title="API for UpChecker",
-              swagger_ui_parameters={"defaultModelsExpandDepth": -1}
+              swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+              openapi_url="/api/openapi.json",
+              docs_url="/api/docs",
+              redoc_url=None
               )
 
 
