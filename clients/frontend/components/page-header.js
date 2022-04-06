604bfe285c29cf1a747afeb4a3a50236e103cf36
badger-py <yan090409@gmail.com>
2022-04-06 19:28:02 +0400

    Footer as component and normal time

--- clients/frontend/components/page-header.js
+++ clients/frontend/components/page-header.js
@@ -1,12 +1,20 @@
 export const page_header = {
     template: ` <header class="page-header">
                     <h2 class="logo_type">UpChecker</h2>
-                    <h2 class="time position-absolute top-50 start-50 translate-middle">{{getCurrentTime()}}</h2>
+                    <h2 class="time position-absolute top-50 start-50 translate-middle">{{time}}</h2>
                     <a href="settings.html"><img src="img/settings.png" alt="Settings" class="settings-icon"></a>
                     <!-- TODO: pencil -->
                 </header>`,
+    data() {
+        return {
+            time: "00:00"
+        }
+    },
+    mounted() {
+        setInterval(() => this.setTime(), 1000)
+    },
     methods: {
-        getCurrentTime() {
+        setTime() {
             const current = new Date();
 
             const hours = current.getHours();
@@ -14,9 +22,7 @@ export const page_header = {
             if (minutes.length == 1) {
                 var minutes = "0" + minutes
             }
-            const time = hours + ":" + minutes;
-
-            return time;
+            this.time = hours + ":" + minutes;
         }
     }
 }
